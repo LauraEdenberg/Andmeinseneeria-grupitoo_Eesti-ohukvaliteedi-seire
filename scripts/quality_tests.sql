4f93a29949b4bb241f467538b86d9a6ba59e5bf8
AnniBurk <annipupart@gmail.com>
2026-06-02 11:53:02 +0300

    Update quality_tests.sql

--- scripts/quality_tests.sql
+++ scripts/quality_tests.sql
@@ -10,56 +10,93 @@ CREATE TABLE IF NOT EXISTS quality.test_results (
 
 TRUNCATE TABLE quality.test_results;
 
-WITH test_cases AS (
+WITH latest_run AS (
+    SELECT run_id
+    FROM staging.pipeline_runs
+    WHERE status = 'success'
+    ORDER BY fetched_at DESC
+    LIMIT 1
+),
+test_cases AS (
     SELECT 
-	'dim_location_has_rows' AS test_name,
-    CASE 
-    	WHEN (SELECT COUNT(*) FROM mart.dim_location) > 0 THEN 0
-        ELSE 1
-        END AS failed_rows,
-    'Asukohtade dimensioonis peab olema vähemalt üks rida.' AS message
+	    'dim_location_has_rows' AS test_name,
+        CASE 
+    	    WHEN (SELECT COUNT(*) FROM mart.dim_location) > 0 THEN 0
+            ELSE 1
+            END AS failed_rows,
+        'Asukohtade dimensioonis peab olema vähemalt üks rida.' AS message
 
     UNION ALL
 
     SELECT 
-	'dim_parameter_has_rows' AS test_name,
-    CASE 
-    	WHEN (SELECT COUNT(*) FROM mart.dim_parameter) > 0 THEN 0
-        ELSE 1
-        END AS failed_rows,
-    'Parameetrite dimensioonis peab olema vähemalt üks rida.' AS message
+	    'dim_parameter_has_rows' AS test_name,
+        CASE 
+    	    WHEN (SELECT COUNT(*) FROM mart.dim_parameter) > 0 THEN 0
+            ELSE 1
+         END AS failed_rows,
+        'Parameetrite dimensioonis peab olema vähemalt üks rida.' AS message
 
     UNION ALL
 
     SELECT 
-	'dim_sensor_has_rows' AS test_name,
-    CASE 
-    	WHEN (SELECT COUNT(*) FROM mart.dim_sensor) > 0 THEN 0
-        ELSE 1
-        END AS failed_rows,
-    'Sensorite dimensioonis peab olema vähemalt üks rida.' AS message
+	    'dim_sensor_has_rows' AS test_name,
+        CASE 
+    	    WHEN (SELECT COUNT(*) FROM mart.dim_sensor) > 0 THEN 0
+            ELSE 1
+            END AS failed_rows,
+        'Sensorite dimensioonis peab olema vähemalt üks rida.' AS message
 
     UNION ALL
 
     SELECT 
-	'dim_parameter_limits_has_rows' AS test_name,
-    CASE 
-    	WHEN (SELECT COUNT(*) FROM mart.dim_parameter_limits) > 0 THEN 0
-        ELSE 1
-        END AS failed_rows,
-    'Saasteainete piirmäärade dimensioonis peab olema vähemalt üks rida.' AS message
+	    'dim_parameter_limits_has_rows' AS test_name,
+        CASE 
+    	    WHEN (SELECT COUNT(*) FROM mart.dim_parameter_limits) > 0 THEN 0
+            ELSE 1
+            END AS failed_rows,
+        'Saasteainete piirmäärade dimensioonis peab olema vähemalt üks rida.' AS message
 
     UNION ALL
 
     SELECT 
-	'parameter_values_raw_has_rows' AS test_name,
-    CASE 
-    	WHEN (SELECT COUNT(*) FROM staging.parameter_values_raw) > 0 THEN 0
-        ELSE 1
-        END AS failed_rows,
-    'APIst andmete laadimisel peab olema vähemalt üks rida.' AS message
+	    'parameter_values_raw_has_rows' AS test_name,
+        CASE 
+    	    WHEN EXISTS(
+                    SELECT 1 
+                    FROM staging.parameter_values_raw AS p 
+                    INNER JOIN latest_run AS l ON p.run_id = l.run_id
+                    )
+                    THEN 0
+            ELSE 1
+            END AS failed_rows,
+        'Viimasel APIst andmete laadimisel peab olema vähemalt üks rida.' AS message
 
-    -- siia veel teste
+    UNION ALL
+
+    SELECT
+        'check_for_duplicates' AS test_name,
+        COUNT(*)::integer AS failed_rows,
+        'Sama sensori, kuupäeva ja ajavahemiku kohta võib olla üks rida.' AS message
+    FROM (
+        SELECT
+            sensor_id,
+            period_from,
+            COUNT(*) AS row_count
+        FROM staging.parameter_values_raw
+        GROUP BY 
+            sensor_id,
+            period_from
+        HAVING COUNT(*) > 1
+    ) AS duplicates
+
+    UNION ALL
+
+    SELECT 
+        'concentrations_reasonable' AS test_name,
+        COUNT(*)::integer AS failed_rows,
+        'Saasteaine kontsentratsioon ei tohi olla negatiivne' AS message
+    FROM staging.parameter_values_raw
+    WHERE value < 0
 )
 
 INSERT INTO quality.test_results (

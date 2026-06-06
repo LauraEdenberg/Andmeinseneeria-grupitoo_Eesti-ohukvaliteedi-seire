d2e63ecfb288e97ad55c6fbe6715854c4b5f9762
AnniBurk <annipupart@gmail.com>
2026-06-06 20:44:21 +0300

    Update quality_tests.sql

--- scripts/quality_tests.sql
+++ scripts/quality_tests.sql
@@ -113,7 +113,7 @@ test_cases AS (
     SELECT 
         'no_of_exceedances_not_null' AS test_name,
         COUNT(*)::integer AS failed_rows,
-        'Piirmäärade ületamise arv ei tohi olla NULL' AS message
+        'Piirmäärade ületamise arv ei tohi olla NULL.' AS message
     FROM mart.v_limit_exceedances
     WHERE no_of_exceedances IS NULL
 )
